9f49517b9d2255a3eff56c53b481fee7f02f086f
Stoyan Trendafilov <stoyan.trendafilov@gmail.com>
2013-08-04 09:18:06 +0300

    Task 01 - Values

---
+++ TypesVariables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+    class TypesVariables
+    {
+        static void Main()
+        {
+            ushort ushortExample = 52130;
+            sbyte sbyteExample = -115;
+            uint uintExample = 4825932;
+            byte byteExample = 97;
+            short shortExample = -10000;
+            Console.WriteLine("Example for type \"ushort\" is {0}, example for type \"sbyte\" is {1}, \nexample for type \"uint\" is {2}, example for type \"byte\" is {3},\nexample for type \"short\" is {4}", ushortExample, sbyteExample, uintExample, byteExample, shortExample);
+        }
+    }
+
